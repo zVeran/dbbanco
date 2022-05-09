2ba686e901ad12f28f55960c30de8094b334c492
Luiz Gustavo <81664581+zVeran@users.noreply.github.com>
2022-05-09 08:37:13 -0300

    Update querydbBanco.sql

--- querydbBanco.sql
+++ querydbBanco.sql
@@ -48,11 +48,46 @@ insert into tbbanco(codigo, nome)
 values ('1', 'Banco do Brasil'),
 ('104', 'Caixa Econômica Federal'),
 ('801', 'Banco Escola');
+
 insert into tbagencia(Codigo, NumeroAgencia, endereco)
 values ('1', '123', 'Av Paulista, 78'),
 ('104', '159', 'Rua Liberdade, 124'),
 ('801', '401', 'Rua Vinte três. 23'),
 ('801', '435', 'Av Marechal, 68');
+
+insert into tbcliente(cpf, nome, sexo, endereco)
+values('12345678910', 'Enildo', 'M', 'Rua Grande, 75'),
+('12345678911', 'Astrogildo', 'M', 'Rua Pequena, 789'),
+('12345678912', 'Monica', 'F', 'Av Larga, 148'),
+('12345678913', 'Cascâo', 'M', 'Av Principal, 369');
+
+insert into tbconta(numeroConta, saldo, TipoConta, NumeroAgencia)
+values('9876', '456.05','1', '123'),
+('9877', '321.00', '1', '123'),
+('9878', '100.00', '2', '435'),
+('9879', '5589.89', '1', '401');
+
+insert into tbhistorico(cpf, numeroconta, datainicio)
+values('12345678910', '9876', '2001-04-15'),
+('12345678911', '9877', '2011-03-10'),
+('12345678912', '9878', '2021-03-11'),
+('12345678913', '9879', '2000-07-05');
+
+insert into tbtelefone_cliente(cpf, telefone)
+values('12345678910', '912345678'),
+('12345678911', '912345679'),
+('12345678912', '912345680'),
+('12345678913', '912345681');
+alter table tbconta drop column NumAgencia;
+describe tbconta;
+
+alter table tbcliente add e_mail varchar(100);
+select * from tbcliente where nome = 'Monica'; 
+select numeroagencia, endereco from tbagencia; 
+select * from tbcliente where sexo = 'M'; 
+
+
+
+
 select * from tbbanco;
 select * from tbagencia;
-
